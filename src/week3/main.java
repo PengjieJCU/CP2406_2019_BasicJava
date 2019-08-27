b529f5899c0e2e7cb8a9c5cf64d3e74d4863679a
pengj <pengjie573@gmail.com>
2019-08-27 14:34:29 +1000

    initial commit

--- src/week3/main.java
+++ src/week3/main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        Game game = new Game();
+        Game game = new Game(3, 5);
         game.generateSecret();
 
         while (true){
